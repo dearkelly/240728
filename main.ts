e58f4d5cd791181a7e250040c830f2658911c64c
dearkelly <147892143+dearkelly@users.noreply.github.com>
2024-07-28 18:01:58 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(input.lightLevel())
-})
+let 方位 = 0
 basic.forever(function () {
-	
+    方位 = input.compassHeading()
+    if (方位 < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (方位 < 135) {
+        basic.showArrow(ArrowNames.East)
+    } else if (方位 < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (方位 < 315) {
+        basic.showArrow(ArrowNames.West)
+    }
 })
